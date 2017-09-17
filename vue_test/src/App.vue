8eb1d879d34ebeb2827200106d965364ce3961dd
AnubisYe <csyezy@163.com>
2017-09-17 22:54:48 +0800

    aaa

---
+++ vue_test/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"> </h1>
+    <ul>
+    	
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function () {
+  	return {
+  		title: 'this is a todo list',
+  		items: [
+  		{
+  			label: 'coding',
+  			isFinished: false
+  		},
+  		{
+  			label: 'walking',
+  			isFinished: true
+  		}
+  		]
+  	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
